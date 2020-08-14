1379db68d41c4194b5673e606487f869604365a9
João carvalho <joaozinho1807@gmail.com>
2020-08-13 21:06:53 -0300

    REST testado

--- src/main/java/com/joaopedro/cursoionic/CursoionicApplication.java
+++ src/main/java/com/joaopedro/cursoionic/CursoionicApplication.java
@@ -1,7 +1,9 @@
-package com.joaopedro;
+package com.joaopedro.cursoionic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class CursoionicApplication {
