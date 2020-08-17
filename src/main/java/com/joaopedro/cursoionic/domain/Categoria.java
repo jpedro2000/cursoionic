db4b2233abb8bed154fb00add0b5794e48541517
João carvalho <joaozinho1807@gmail.com>
2020-08-17 20:26:35 -0300

    Criando repository e service para classe categoria

--- src/main/java/com/joaopedro/cursoionic/domain/Categoria.java
+++ src/main/java/com/joaopedro/cursoionic/domain/Categoria.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+
 
 @Entity
 public class Categoria implements Serializable{
@@ -14,7 +14,7 @@ public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY )
 	private Integer id;
 	private String nome;
 	
---
+++ src/main/java/com/joaopedro/cursoionic/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.joaopedro.cursoionic.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.joaopedro.cursoionic.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
+
